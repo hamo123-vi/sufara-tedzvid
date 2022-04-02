88dfb5fad6a022966e8afc8bfa9441259fc17217
unknown <amil.valjevac@stu.ibu.edu.ba>
2022-04-02 16:03:49 +0200

    Dektop verzija

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> Sufara i tedžvid - zbirna tabela </title>
+        <link rel="stylesheet" href="src/assets/style/style.css">
+    </head>
+
+    <body>
+        <?php require_once("src/templates/header.php") ?>
+        <main>
+            <div>
+                <img src="src/assets/zbirnatabela.jpg" width="100%" alt="Zbirna tabela" >
+            </div>
+        </main>
+    </body>
+</html>
---
+++ src/templates/header.php
@@ -0,0 +1,12 @@
+<header>
+    <div>
+        <a href="index.php">SUFARA I TEDŽVID</a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Zbirna tabela</a></li>
+            <li><a href="pojedinacno.php">Pojedinačne tabele</a></li>
+            <li><a href="http://localhost/sufara-tedzvid/SufaraEšrefKovačević.pdf">Sufara</a></li>
+        </ul>
+    </nav>
+</header>
