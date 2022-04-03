06b40fb060b4cf2dd1a60ebc0a642eb84ae75ed4
unknown <amil.valjevac@stu.ibu.edu.ba>
2022-04-03 10:28:19 +0200

    Debugging

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,12 +1,12 @@
 <header>
     <div>
-        <a href="index.php">SUFARA I TEDŽVID</a>
+        <a href="index.php">SUFARA I TEDŽVID OSNOVE</a>
     </div>
     <nav>
         <ul>
             <li><a href="index.php">Zbirna tabela</a></li>
             <li><a href="pojedinacno.php">Pojedinačne tabele</a></li>
-            <li><a href="http://localhost/sufara-tedzvid/SufaraEšrefKovačević.pdf">Sufara</a></li>
+            <li><a href="SufaraEšrefKovačević.pdf">Sufara</a></li>
         </ul>
     </nav>
 </header>
