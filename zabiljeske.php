952b9a30901eb468a112a79c654e2b29a8916d41
unknown <amil.valjevac@stu.ibu.edu.ba>
2022-04-16 17:27:36 +0200

    Dopuna zabilješki

--- zabiljeske.php
+++ zabiljeske.php
@@ -11,21 +11,21 @@
                 <h1>POJEDINE ZABILJEŠKE I LIČNI ZAKLJUČCI SA MNOGO DOBRIH YOU TUBE VIDEA</h1>
                 <br><br><br>
                 <h3>MOJ ZAKLJUČAK OD 1-5 ČASA PROF.MUHAMED NASIR</h3>
-                <img alt="Predavanja 1-5" src="src/assets/MN1-5.jpg">
+                <img alt="Predavanja 1-5" src="src/assets/R2.jpg">
                 <br><br><br>
                 <h3>MEDOVI SAŽETAK OD 6-8 ČASA PROF.MUHAMED NASIR</h3>
-                <img alt="Predavanja 6-8" src="src/assets/MN6-8.jpg">
-                <br><br><br>
-                <h3>TEDŽVIDSKO PRAVILO SLOVA R PROF.MUHAMED NASIR</h3>
-                <img alt="Pravilo R" src="src/assets/R1.jpg">
-                <br>
-                <br>
-                <img alt="Pravilo R" src="src/assets/R2.jpg">
+                <img alt="Predavanja 6-8" src="src/assets/R1.jpg">
                 <br><br><br>
                 <h3>HEMZE PROF.MUHAMED NASIR</h3>
                 <img alt="Pravilo R" src="src/assets/H.jpg">
                 <br><br><br>
-                <h3>PREZENTACIJA ZNAKOVA ZA ČITANJE U KUR-ANU OD MUSLIMSLAND</h3>
+                <h3>TEDŽVIDSKO PRAVILO SLOVA R PROF.MUHAMED NASIR</h3>
+                <img alt="Pravilo R" src="src/assets/MN1-5.jpg">
+                <br>
+                <br>
+                <img alt="Pravilo R" src="src/assets/MN6-8.jpg">
+                <br><br><br>
+                <h3>PREZENTACIJA ZNAKOVA ZA ČITANJE U KUR'ANU OD MUSLIMSLAND</h3>
                 <img alt="Prezentacija znakova" src="src/assets/P1.jpg">
                 <br>
                 <br>
@@ -41,7 +41,16 @@
                 <img alt="Arapski jezik" src="src/assets/A1.jpg">
                 <br>
                 <br>
-                <img alt="Arapski jezik" src="src/assets/A2.jpg">
+                <h3>BISMILA</h3>
+                <img alt="Arapski jezik" src="src/assets/bismila.jpg">
+                <br>
+                <br>
+                <br>
+                <h3>VIŠE DOBRIH TEMA</h3>
+                <br>
+                <br>
+                <br>
+                <h3>Za više dobrih tema posjetite <a style="color: blue" href="https://m.facebook.com/Sufara-i-ted%C5%BEvid-osnove-testna-verzija-1992735521023625/">Facebook</a> stranicu</h3>
             </div>
         </main>
         <script type="text/javascript" src="src/assets/js/activeToggler.js"></script>
